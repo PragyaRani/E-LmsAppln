8159e6ecf802d8fe8d0de2809077d51b1e2e83d9
cogdotnet1659 <cogdotnet1659@CTSDOTNET147>
2022-10-29 18:42:22 +0530

    firstcommit

---
+++ Microservice.WebApi/User.Microservice/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using User.Microservice.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace User.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/<UserController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<UserController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<UserController>
+        [HttpPost("signIn")]
+        public object Post([FromBody] UserSignIn user)
+        {
+            return Ok("User logged successfully");
+        }
+        [HttpPost("signUn")]
+        public object Register([FromBody] UserSignUp user)
+        {
+            return Ok("User resgistered successfully");
+        }
+
+        //// PUT api/<UserController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<UserController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Microservice.WebApi/User.Microservice/Models/UserSignUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace User.Microservice.Models
+{
+    public class UserSignUp
+    {
+        [Required(ErrorMessage = "name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "address is required")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "city is required")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "pin is required")]
+        public int Pin { get; set; }
+        [Required(ErrorMessage = "state is required")]
+        public string State { get; set; }
+        [Required(ErrorMessage = "contact is required")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "email is required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "password is required")]
+        public string Password { get; set; }
+    }
+}
